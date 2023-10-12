342ace94a2401dd25f484c3dcb194c94b330d1bc
Nwamanna <nwamannazika@gmail.com>
2023-10-12 13:30:10 +0000

    edit file to fix bug

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -12,7 +12,7 @@ CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
     UPDATE users
     SET average_score = (
-        SELECT AVG(score) FROM corrections WHERE user_id = user_id
+        SELECT AVG(score) FROM corrections c WHERE c.user_id = user_id
     )
     WHERE id = user_id;
 END $$
