68a842f3a90ad6901adef07ee827643fe4ca2dd0
Nwamanna <nwamannazika@gmail.com>
2023-10-12 14:18:26 +0000

    fix typo

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,2 +1,2 @@
 -- Create the index idx_name_first on the first letter of the name column
-CREATE INDEX idx_name_first ON names (name(1), score);
+CREATE INDEX idx_name_first ON names (LEFT(name, 1), score);
